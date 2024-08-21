b74ac5045b78d29210d540c06b5f1f3eec487217
David Forero <davidforero717@gmail.com>
2024-08-21 07:15:47 -0500

    diferecia de mono y webFLux

--- demo/demo/src/main/java/com/example/demo/DemoReactorApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoReactorApplication.java
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoReactorApplication implements CommandLineRunner {
@@ -19,14 +23,39 @@ public class DemoReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        reactor();
+        mono();
+        flux();
+        fluxMono();
     }
 
-    public void reactor() {
+    public void mono() {
+        System.out.println("mono");
         Mono.just(new Persona(1, "David", 25))
                 .doOnNext(persona -> {
                     persona.setEdad(persona.getEdad() + 6);
                 })
                 .subscribe(p -> log.info(p.toString()));
+    }
+
+    public void flux(){
+        System.out.println("flux");
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "Camilo", 10));
+        personas.add(new Persona(2, "Cristian", 30));
+        personas.add(new Persona(3, "Caren", 15));
+        personas.add(new Persona(4, "Luisa", 20));
+
+        Flux.fromIterable(personas).subscribe(p -> log.info(p.toString()));
+    }
+
+    public void fluxMono(){
+        System.out.println("fluxMono");
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "Camilo", 10));
+        personas.add(new Persona(2, "Cristian", 30));
+        personas.add(new Persona(3, "Caren", 15));
+        personas.add(new Persona(4, "Luisa", 20));
+
+        Flux.fromIterable(personas).collectList().subscribe(p -> log.info(p.toString()));
     }
    }
